64cb4724ef6622787370e9c4a6c38597c31058a2
diegoami <diego.amicabile@gmail.com>
2017-08-30 21:55:54 +0200

    Convert all to audios - second version

--- recommender/convert_to_audios.py
+++ recommender/convert_to_audios.py
@@ -1,15 +1,32 @@
 from oauth2client.tools import argparser
 
-def conv_to_audio(inputDir, inputFile, outputFile):
-    clip = mp.VideoFileClip(inputDir+inputFile)
+import glob
+import os
+import itertools
+
+def conv_to_audio(inputFile, outputFile):
+    clip = mp.VideoFileClip(inputFile)
     clip.audio.write_audiofile(outputFile)
+
 
 if __name__ == "__main__":
     import moviepy.editor as mp
 
     argparser.add_argument('--inputDir')
-    argparser.add_argument('--inputFile')
-    argparser.add_argument('--outputFile')
+    argparser.add_argument('--outputDir')
 
     args = argparser.parse_args()
-    conv_to_audio(args.inputDir or "", args.inputFile, args.outputFile)
+
+    filenames1 = list(glob.iglob(args.inputDir + '/*.mp4'))
+    filenames2 = list(glob.iglob(args.inputDir + '/*.webm'))
+    filenames3 = list(glob.iglob(args.inputDir + '/*.mkv'))
+    filenames = filenames1 + filenames2 + filenames3
+    for filename in filenames:
+        print(filename)
+        basename = os.path.basename(filename)
+        file_root, file_extension = os.path.splitext(basename )
+        #print(file_root)
+        ouputaudiofile = args.outputDir+'/'+file_root+".mp3"
+        conv_to_audio(filename, ouputaudiofile )
+    #conv_to_audio(args.inputDir or "", args.inputFile, args.outputFile)
+
