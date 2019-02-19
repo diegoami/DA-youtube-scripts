73d874be7faca6a392ebe863f78405deffda659b
Diego Amicabile <diego.amicabile@gmail.com>
2019-02-19 23:20:59 +0100

    related videos

--- youtube-scripts/retrieve_video_info.py
+++ youtube-scripts/retrieve_video_info.py
@@ -1,16 +1,16 @@
-from youtube3.youtube import *
+from youtube3 import YoutubeClient
 from oauth2client.tools import argparser
-
+import os
 if __name__ == "__main__":
     argparser.add_argument('--videoId')
     args = argparser.parse_args()
     if args.videoId == None:
         print("required argument --videoId <videoId>")
     else:
-        youtube = Youtube(get_authenticated_service(args))
-        videoInfo = youtube.get_video(args.videoId )
-        channelId = youtube.get_channel_id(args.videoId )
-        relatedVideos = youtube.get_related_videos(args.videoId )
+        youtube_client = YoutubeClient(os.path.join(os.path.dirname(__file__), 'client_secrets.json'))
+        videoInfo = youtube_client .get_video(args.videoId )
+        channelId = youtube_client .get_channel_id(args.videoId )
+        relatedVideos = youtube_client .get_related_videos(args.videoId )
         print(videoInfo)
         print(channelId)
         print(relatedVideos)
